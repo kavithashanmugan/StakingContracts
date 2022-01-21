a49731642f5e5ce8d7ae159ee115dd77b19ff116
Kavitha Shanmugan <44939239+kavithashanmugan@users.noreply.github.com>
2022-01-21 17:17:43 +0530

    Create StakingRewards.sol

---
+++ StakingRewards.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+pragma abicoder v2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/Math.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol";
+
+interface IMintableToken is IERC20 {
+    function mint(address _receiver, uint256 _amount) external;
+}
+contract StakingRewards {
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+
+    uint public rewardRate = 100;
+    uint public lastUpdateTime;
+    uint public rewardPerTokenStored;
+
+    mapping(address => uint) public userRewardPerTokenPaid;
+    mapping(address => uint) public rewards;
+
+    uint private _totalSupply;
+    mapping(address => uint) private _balances;
+
+    constructor(address _stakingToken) {
+        stakingToken = IMintableToken(_stakingToken);
+        rewardsToken = IMintableToken(_stakingToken);
+    }
+
+    function rewardPerToken() public view returns (uint) {
+        if (_totalSupply == 0) {
+            return 0;
+        }
+        return
+            rewardPerTokenStored +
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / _totalSupply);
+    }
+
+    function earned(address account) public view returns (uint) {
+        return
+            ((_balances[account] *
+                (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18) +
+            rewards[account];
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        _;
+    }
+
+    function stake(uint _amount) external updateReward(msg.sender) {
+        _totalSupply += _amount;
+        _balances[msg.sender] += _amount;
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint _amount) external updateReward(msg.sender) {
+        _totalSupply -= _amount;
+        _balances[msg.sender] -= _amount;
+        stakingToken.transfer(msg.sender, _amount);
+    }
+
+    function getReward() external updateReward(msg.sender) {
+        uint reward = rewards[msg.sender];
+        rewards[msg.sender] = 0;
+        rewardsToken.transfer(msg.sender, reward);
+    }
+}
